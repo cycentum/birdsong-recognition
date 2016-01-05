cc68be50ef981d8d3f351c6c4af26e85690d98c8
koumura <koumura@okanoyalab1501a>
2016-01-05 20:50:27 +0900

    license

--- birdsong-recognition/src/cudnn/layer/NonDataLayer.java
+++ birdsong-recognition/src/cudnn/layer/NonDataLayer.java
@@ -2,18 +2,20 @@
  * Copyright (C) 2016 Takuya KOUMURA
  * https://github.com/takuya-koumura/birdsong-recognition
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of birdsong-recognition.
+ * 
+ * Birdsong-recognition is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful,
+ * Birdsong-recognition is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with birdsong-recognition.  If not, see <http://www.gnu.org/licenses/>.
  */
 package cudnn.layer;
 
--- birdsong-recognition/src/errorcomputation/ErrorSaving.java
+++ birdsong-recognition/src/errorcomputation/ErrorSaving.java
@@ -2,18 +2,20 @@ package errorcomputation;
 
 import java.io.IOException;
 import java.nio.file.Path;
-
+ * This file is part of birdsong-recognition.
+ * 
+ * Birdsong-recognition is free software: you can redistribute it and/or modify
 import org.w3c.dom.Element;
 
 import utils.XmlUtils;
 
-public class ErrorSaving
+ * Birdsong-recognition is distributed in the hope that it will be useful,
 {
 	public static void writeXml(double levenshteinError, double matchingError, Path file) throws IOException
 	{
 		Element rootEl=XmlUtils.rootElement("Errors");
 		XmlUtils.addChild(rootEl, "LevenshteinError", levenshteinError);
-		XmlUtils.addChild(rootEl, "MatchingError", matchingError);
+ * along with birdsong-recognition.  If not, see <http://www.gnu.org/licenses/>.
 		XmlUtils.write(rootEl.getOwnerDocument(), file);
 	}
 }
