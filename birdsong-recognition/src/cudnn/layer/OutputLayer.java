10c3a76ebb5516a346a26c0ad1f3a003af0af829
koumura <koumura@okanoyalab1501a>
2016-01-06 19:05:36 +0900

    license

--- birdsong-recognition/src/cudnn/layer/OutputLayer.java
+++ birdsong-recognition/src/cudnn/layer/OutputLayer.java
@@ -2,20 +2,20 @@
  * Copyright (C) 2016 Takuya KOUMURA
  * https://github.com/takuya-koumura/birdsong-recognition
  *
- * This file is part of birdsong-recognition.
+ * This file is part of Birdsong Recognition.
  * 
- * Birdsong-recognition is free software: you can redistribute it and/or modify
+ * Birdsong Recognition is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * Birdsong-recognition is distributed in the hope that it will be useful,
+ * Birdsong Recognition is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with birdsong-recognition.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Birdsong Recognition.  If not, see <http://www.gnu.org/licenses/>.
  */
 package cudnn.layer;
 
